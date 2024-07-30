ed16fd076cd999b2cbf733258d0188fe0a6b2685
Ammar Khan <ammar.khaan05@gmail.com>
2024-07-31 00:49:28 +0500

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,179 @@
+"use client"
+import { Box, Stack, Typography, Button, Modal, TextField } from "@mui/material";
+import { firestore } from "@/firebase";
+import { collection, getDoc, getDocs, query, setDoc,doc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+  gap: 3,
+};
+
+const item = [
+  "Rice",
+  "Quinoa",
+  "Pasta",
+  "Couscous",
+  "Oats",
+  "Canned tomatoes",
+  "Canned beans",
+  "Canned vegetables",
+  "Canned fruit",
+  "Canned tuna",
+  "Beef Broth",
+  "Flour",
+  "Sugar",
+  "Baking powder",
+  "Baking soda",
+  "Yeast",
+  "Cornstarch",
+  "Cocoa powder",
+  "Olive oil",
+  "Vegetable oil",
+  "Coconut oil",
+  "Sesame oil",
+  "Apple cider vinegar",
+  "White vinegar",
+  "Balsamic vinegar",
+  "Salt",
+  "Black pepper",
+  "Garlic powder",
+  "Onion powder",
+  "Paprika",
+  "Chili powder",
+  "Cumin",
+  "Oregano",
+  "Basil",
+  "Thyme",
+  "Cinnamon",
+  "Nutmeg",
+  "Bay leaves",
+  "Soy sauce",
+  "Worcestershire sauce",
+  "Hot sauce",
+  "Mustard",
+  "Ketchup",
+  "Mayonnaise",
+  "Barbecue sauce",
+  "Honey",
+  "Maple syrup",
+  "Peanut butter",
+  "Jam",
+  "Crackers",
+  "Popcorn kernels",
+  "Nuts",
+  "Dried fruit",
+  "Granola bars",
+  "Lentils",
+  "Split peas",
+  "Dried beans",
+  "Dried pasta",
+  "Tea bags",
+  "Coffee",
+  "Cereal",
+  "Bread crumbs",
+  "Powdered milk",
+  "Instant mashed potatoes"
+]
+
+export default function Home() {
+  const [pantry, setPantry] = useState([])
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const [itemName, setItemName] = useState('')
+
+  const updatePantry = async () => {
+    const snapshots = query(collection(firestore, 'pantry'))
+    const docs = await getDocs(snapshots) 
+    const pantryList = []
+    docs.forEach((doc) => {
+      pantryList.push(doc.id)
+    })
+    console.log(pantryList)
+    setPantry(pantryList)
+  }
+
+  useEffect(() => {
+    updatePantry()
+  }, [])
+
+const addItem = async (item) => {
+  const docRef = doc(collection(firestore,'pantry'),item)
+  await setDoc(docRef, {})
+  updatePantry()
+}
+
+  return <Box
+  width="100vw"
+  height="100vh"
+  display={"flex"}
+  justifyContent={"center"}
+  flexDirection={"column"}
+  alignItems={"center"}
+  gap={2}
+  >
+    <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Add item
+          </Typography>
+          <Stack width="100%" direction={'row'} spacing={2}>
+          <TextField id="outlined-basic" label="Item" variant="outlined" fullWidth value={itemName} onChange={(e) => setItemName(e.target.value)}/>
+          <Button variant="contained" 
+          onClick={() => {
+            addItem(itemName) 
+            setItemName('')
+            handleClose()}}
+            >Add</Button>
+        </Stack>
+        </Box>
+      </Modal> 
+    <Button variant="outlined" onClick={handleOpen}>Add</Button> 
+    <Box border={'1px solid #333'}>
+      <Box width="800px" height="100px" bgcolor={'#ADD8E6'} display={"flex"} justifyContent={"center"} alignItems={"center"}>
+        <Typography variant={'h2'} color={'#333'} textAlign={'center'}>
+          Pantry Items
+        </Typography>
+      </Box>
+      <Stack width="800px" height="500px" spacing={2} overflow={'auto'}>
+        {pantry.map((i) => (
+          <Box
+            key={i}
+            width="100%"
+            minHeight="150px"
+            display={"flex"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            bgcolor={'#f0f0f0'}
+          >
+            <Typography 
+              variant="h3"
+              color={'#333'}
+              textAlign={'center'}
+            >
+              {
+                i.charAt(0).toUpperCase() + i.slice(1)
+              }
+            </Typography>
+          </Box>
+        ))}
+      </Stack>
+    </Box>
+  </Box>
+}
