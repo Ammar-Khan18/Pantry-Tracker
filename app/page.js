796adb9bca2fbdff302334779ba348d5cabb8547
Ammar Khan <ammar.khaan05@gmail.com>
2024-07-31 20:58:12 +0500

    added an app bar

--- app/page.js
+++ app/page.js
@@ -1,8 +1,9 @@
 "use client"
-import { Box, Stack, Typography, Button, Modal, TextField } from "@mui/material";
+import { Box, Stack, Typography, Button, Modal, TextField, AppBar, Toolbar, IconButton } from "@mui/material";
 import { firestore } from "@/firebase";
 import { collection, getDoc, getDocs, query, setDoc,doc, deleteDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
+import InventoryIcon from '@mui/icons-material/Inventory';
 
 const style = {
   position: 'absolute',
@@ -68,73 +69,73 @@ const removeItem = async (item) => {
   await updatePantry()
 }
 
-  return( <Box
-  width="100vw"
-  height="100vh"
-  display={"flex"}
-  justifyContent={"center"}
-  flexDirection={"column"}
-  alignItems={"center"}
-  gap={2}
-  >
-    <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Add item
+  return( 
+    <Box
+    width="100vw"
+    height="100vh"
+    display={"flex"}
+    justifyContent={"center"}
+    flexDirection={"column"}
+    alignItems={"center"}
+    gap={2}>
+      {/*App Bar*/}
+      <AppBar position="static" sx={{ mb: 1 }}>
+        <Toolbar>
+          <InventoryIcon sx={{ mr: 2 }} />
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Pantry Buddy
           </Typography>
-          <Stack width="100%" direction={'row'} spacing={2}>
-          <TextField id="outlined-basic" label="Item" variant="outlined" fullWidth value={itemName} onChange={(e) => setItemName(e.target.value)}/>
-          <Button variant="contained" 
-          onClick={() => {
-            addItem(itemName) 
-            setItemName('')
-            handleClose()}}
-            >Add</Button>
+        </Toolbar>
+      </AppBar>
+      {/*Input Field for items*/}
+      <Box>
+        <Stack width="100%" direction={'row'} spacing={2}>
+        <TextField label="Item" variant="outlined" fullWidth value={itemName} onChange={(e) => setItemName(e.target.value)}/>
+        <Button variant="contained" 
+        onClick={() => {
+          addItem(itemName) 
+          setItemName('')
+          handleClose()}}
+          >Add</Button>
         </Stack>
-        </Box>
-      </Modal> 
-    <Button variant="outlined" onClick={handleOpen}>Add</Button> 
-    <Box border={'1px solid #333'}>
-      <Box width="800px" height="100px" bgcolor={'#ADD8E6'} display={"flex"} justifyContent={"center"} alignItems={"center"}>
-        <Typography variant={'h2'} color={'#333'} textAlign={'center'}>
-          Pantry Items
-        </Typography>
       </Box>
-      <Stack width="800px" height="500px" spacing={2} overflow={'auto'}>
-        {pantry.map(({name, count}) => (
-            <Box
-              key={name}
-              width="100%"
-              minHeight="150px"
-              display={"flex"}
-              justifyContent={"space-between"}
-              alignItems={"center"}
-              bgcolor={'#f0f0f0'}
-              paddingX={5}>
-              <Typography 
-                variant="h3"
-                color={'#333'}
-                textAlign={'center'}>
-                {
-                  name.charAt(0).toUpperCase() + name.slice(1)
-                }
-              </Typography>
-              <Typography variant="h3" color={'#333'} textAlign={'center'}>
-                Quantity: {count}
-              </Typography>
-            <Stack direction={'row'} spacing={2}>
-              <Button variant="contained" onClick={() => addItem(name)}>Add</Button>
-              <Button variant="contained" onClick={() => removeItem(name)}>Remove</Button>
-            </Stack>
-          </Box>
-        ))}
-      </Stack>
+      
+      <Box border={'1px solid #333'}>
+        <Box width="800px" height="100px" bgcolor={'#ADD8E6'} display={"flex"} justifyContent={"center"} alignItems={"center"}>
+          <Typography variant={'h2'} color={'#333'} textAlign={'center'}>
+            Pantry Items
+          </Typography>
+        </Box>
+        <Stack width="800px" height="500px" spacing={2} overflow={'auto'}>
+          {pantry.map(({name, count}) => (
+              <Box
+                key={name}
+                width="100%"
+                minHeight="150px"
+                display={"flex"}
+                justifyContent={"space-between"}
+                alignItems={"center"}
+                bgcolor={'#f0f0f0'}
+                paddingX={5}>
+                <Typography 
+                  variant="h3"
+                  color={'#333'}
+                  textAlign={'center'}>
+                  {
+                    name.charAt(0).toUpperCase() + name.slice(1)
+                  }
+                </Typography>
+                <Typography variant="h3" color={'#333'} textAlign={'center'}>
+                  Quantity: {count}
+                </Typography>
+              <Stack direction={'row'} spacing={2}>
+                <Button variant="contained" onClick={() => addItem(name)}>Add</Button>
+                <Button variant="contained" onClick={() => removeItem(name)}>Remove</Button>
+              </Stack>
+            </Box>
+          ))}
+        </Stack>
+      </Box>
     </Box>
-  </Box>
   )
 }
