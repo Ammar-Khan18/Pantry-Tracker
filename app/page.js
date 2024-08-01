05b80b3ce16686a2f707aa8063171122460c5d74
Ammar Khan <ammar.khaan05@gmail.com>
2024-08-02 01:54:17 +0500

    added a header and footer made the site presentable added all my personal links

--- app/page.js
+++ app/page.js
@@ -1,11 +1,17 @@
 "use client"
-import { Box, Stack, Typography, Button, Modal, TextField, AppBar, Toolbar, IconButton } from "@mui/material";
+import { Box, Stack, Typography, Button, TextField, Toolbar, IconButton, styled, Container} from "@mui/material";
 import { firestore } from "@/firebase";
 import { collection, getDoc, getDocs, query, setDoc,doc, deleteDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import InventoryIcon from '@mui/icons-material/Inventory';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import { createTheme, makeStyles, ThemeProvider} from '@mui/material/styles';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import WebIcon from '@mui/icons-material/Web';
 
 const style = {
   position: 'absolute',
@@ -19,6 +25,51 @@ const style = {
   p: 4,
   gap: 3,
 };
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#d5d5d6',
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      main: '#FF0000',
+    },
+    error: {
+      main: '#dc2626',
+    },
+  },
+  components: {
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          borderRadius: '4px',
+        },
+      },
+    },
+  },
+});
+
+const Header = styled(Box)(({ theme }) => ({
+  backgroundColor: '#e6e6e6',
+  color: '#000000',
+  padding: theme.spacing(1, 2),
+  position: 'relative',
+  width: '100%',
+  padding: 10,
+}));
+
+const HeaderContent = styled(Container)(({ theme }) => ({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  padding: 0,
+  margin: 0,
+  [theme.breakpoints.down('sm')]: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+}));
 
 export default function Home() {
   const [pantry, setPantry] = useState([])
@@ -71,78 +122,76 @@ const removeItem = async (item) => {
   await updatePantry()
 }
 
-  return( 
-    <Box
-    width="100vw"
-    height="100vh"
-    display={"flex"}
-    justifyContent={"center"}
-    flexDirection={"column"}
-    alignItems={"center"}
-    gap={2}>
-      {/*App Bar*/}
-      <AppBar position="static" sx={{ mb: 1 }}>
-        <Toolbar>
+  return(
+    <ThemeProvider theme={theme}>
+      {/*Header*/}
+      <Header>
+        <HeaderContent maxWidth='2000'>
           <InventoryIcon sx={{ mr: 2 }} />
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Pantry Buddy
           </Typography>
-        </Toolbar>
-      </AppBar>
-      {/*Input Field for items*/}
-      <Box>
-        <Stack width="100%" direction={'row'} spacing={2}>
-        <TextField label="Item" variant="outlined" fullWidth value={itemName} onChange={(e) => setItemName(e.target.value)}/>
-        <Button variant="contained" 
-        onClick={() => {
-          addItem(itemName) 
-          setItemName('')
-          handleClose()}}
-          >Add</Button>
-        </Stack>
-      </Box>
-      
-      <Box border={'1px solid #333'}>
-        <Box width="800px" height="100px" bgcolor={'#ADD8E6'} display={"flex"} justifyContent={"center"} alignItems={"center"}>
-          <Typography variant={'h2'} color={'#333'} textAlign={'center'}>
-            Pantry Items
-          </Typography>
-        </Box>
-        <Stack width="800px" height="500px" spacing={2} overflow={'auto'}>
-          {pantry.map(({name, count}) => (
-              <Box
-                key={name}
-                width="100%"
-                minHeight="150px"
-                display={"flex"}
-                justifyContent={"space-between"}
-                alignItems={"center"}
-                bgcolor={'#f0f0f0'}
-                paddingX={5}>
-                <Typography variant="h6">{name.charAt(0).toUpperCase() + name.slice(1)}</Typography>
-                  <Box display="flex" alignItems="center">
-                    <IconButton
-                      color="primary"
-                      onClick={() => addItem(name)}
-                      sx={{ mx: 1 }}
-                    >
-                      <AddIcon />
-                    </IconButton>
-                    <Typography variant="body1" sx={{ mx: 2 }}>
-                      {count}
-                    </Typography>
-                    <IconButton
-                      color="secondary"
-                      onClick={() => removeItem(name)}
-                      sx={{ mx: 1 }}
-                    >
-                      <RemoveIcon />
-                    </IconButton>
+        </HeaderContent>
+      </Header>
+      <Box width="100vw" height="100vh" display={"flex"} justifyContent={"center"} flexDirection={"column"} alignItems={"center"} bgcolor={'#3d3d43'}>
+        <Box display={'flex'} flexDirection={{xs:'row', md:'column'}} gap={2}>
+          {/*Input Field for items*/}
+          <Card borderradius={4} boxShadow={'0 4px 8px rgba(0,0,0,0.1)'} sx={{bgcolor: '#e6e6e6'}}>
+            <CardContent>
+              <Stack width="100%" direction={'row'} spacing={2}>
+              <TextField label="Enter Item Name" variant="outlined" fullWidth value={itemName} onChange={(e) => setItemName(e.target.value)}/>
+              <Button variant="contained" sx={{backgroundColor: '#d5d5d6', color: '#000000'}}
+              onClick={() => {
+                addItem(itemName) 
+                setItemName('')
+                handleClose()}}
+                >Add</Button>
+              </Stack>
+            </CardContent> 
+          </Card>
+          {/*Pantry Items Display*/}
+          <Card borderradius={4} boxShadow={'0 4px 8px rgba(0,0,0,0.1)'} sx={{bgcolor: '#e6e6e6'}}>
+            <CardContent>
+              <Box width="800px" height="85px" bgcolor={'#0f0f10'} display={"flex"} justifyContent={"center"} alignItems={"center"}>
+                <Typography variant={'h2'} color={'#ffffff'} textAlign={'center'}>
+                  Pantry Items
+                </Typography>
+              </Box>
+              <Stack width="800px" height="500px" spacing={2} overflow={'auto'}>
+                {pantry.map(({name, count}) => (
+                    <Box key={name} width="100%" minHeight="40px" display={"flex"} justifyContent={"space-between"} alignItems={"center"} bgcolor={'#e6e6e6'} color={'#000000'} paddingX={5}>
+                      <Typography variant="h6">{name.charAt(0).toUpperCase() + name.slice(1)}</Typography>
+                      <Box display="flex" alignItems="center">
+                        <IconButton onClick={() => addItem(name)} sx={{ mx: 1, color: '#008000' }}>
+                          <AddIcon />
+                        </IconButton>
+                        <Typography variant="body1" sx={{ mx: 2 }}> {count} </Typography>
+                        <IconButton onClick={() => removeItem(name)} sx={{ mx: 1, color: '#FF0000' }} >
+                          <RemoveIcon />
+                        </IconButton>
+                      </Box>
                   </Box>
-            </Box>
-          ))}
-        </Stack>
+                ))}
+              </Stack>
+            </CardContent>
+          </Card>
+        </Box>
       </Box>
-    </Box>
+      {/*Footer*/}
+      <Box sx={{backgroundColor: '#3d3d43', color: '#000000', spacing: 2, bottom: 0, width: '100%'}} padding={2}>
+        <Typography variant="h6" textAlign={'left'}> © Pantry Buddy 2024 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .Made by Ammar Khan</Typography>
+        <Box sx={{backgroundColor: '#3d3d43', color: '#000000', bottom: 0, width: '100%'}} padding={2} justifyContent={'center'} display={'flex'}>
+            <a href="https://github.com/Ammar-Khan18" style={{ display: 'flex', alignItems: 'center', color: 'inherit', textDecoration: 'none' }}>
+              <GitHubIcon />
+            </a>
+            <a href="https://www.linkedin.com/in/ammarbinaamirkhan/" style={{ display: 'flex', alignItems: 'center', color: 'inherit', textDecoration: 'none' }}>
+              <LinkedInIcon />
+            </a>
+            <a href="https://ammar-khan18.github.io/Portfolio-Website/" style={{ display: 'flex', alignItems: 'center', color: 'inherit', textDecoration: 'none' }}>
+              <WebIcon />
+            </a>
+        </Box>
+      </Box>
+    </ThemeProvider>
   )
 }
