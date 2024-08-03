4d4a4340c6520ea397c0e838d1eaa4fe17bea56d
Ammar Khan <ammar.khaan05@gmail.com>
2024-08-03 23:59:37 +0500

    tried to make it responsive

--- app/pantry/page.js
+++ app/pantry/page.js
@@ -8,13 +8,11 @@ import RemoveIcon from '@mui/icons-material/Remove';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import { createTheme, makeStyles, ThemeProvider} from '@mui/material/styles';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import WebIcon from '@mui/icons-material/Web';
 import { signOut, onAuthStateChanged } from 'firebase/auth';
 import { firestore, auth } from '@/firebase';
 import { useRouter } from 'next/navigation';
 import withAuth from '../protectedRoute';
+import { GitHub, LinkedIn, Public } from '@mui/icons-material';
 
 const style = {
   position: 'absolute',
@@ -172,62 +170,65 @@ const ToHome = () => {
           </Typography>
         </HeaderContent>
       </Header>
-      <Box width="100vw" height="100vh" display={"flex"} justifyContent={"center"} flexDirection={"column"} alignItems={"center"} backgroundColor={'#16213e'}>
-        <Box display={'flex'} flexDirection={{xs:'row', md:'column'}} gap={2}>
-          {/*Input Field for items*/}
-          <Card borderradius={4} boxShadow={'0 4px 8px rgba(0,0,0,0.1)'} sx={{bgcolor: '#2D2D2D'}}>
-            <CardContent>
-              <Stack width="100%" direction={'row'} spacing={2}>
-              <TextField label="Enter Item Name" variant="outlined" fullWidth value={itemName} onChange={(e) => setItemName(e.target.value)}/>
-              <Button variant="contained" color='secondary' sx={{color: '#000000'}}
-              onClick={() => {
-                addItem(itemName) 
-                setItemName('')
-                handleClose()}}
-                >Add</Button>
-              </Stack>
-            </CardContent> 
-          </Card>
-          {/*Pantry Items Display*/}
-          <Card borderradius={4} boxShadow={'0 4px 8px rgba(0,0,0,0.1)'} sx={{bgcolor: '#2D2D2D'}}>
-            <CardContent>
-              <Box width="800px" height="85px" bgcolor={'#0f0f10'} display={"flex"} justifyContent={"center"} alignItems={"center"}>
-                <Typography variant={'h2'} color={'#ffffff'} textAlign={'center'}>
-                  Pantry Items
-                </Typography>
-              </Box>
-              <Stack width="800px" height="500px" spacing={2} overflow={'auto'}>
-                {pantry.map(({name, count}) => (
-                    <Box key={name} width="100%" minHeight="40px" display={"flex"} justifyContent={"space-between"} alignItems={"center"} bgcolor={'#2D2D2D'} color={'#ffffff'} paddingX={5}>
-                      <Typography variant="h6">{name.charAt(0).toUpperCase() + name.slice(1)}</Typography>
-                      <Box display="flex" alignItems="center">
-                        <IconButton onClick={() => addItem(name)} sx={{ mx: 1, color: '#008000' }}>
-                          <AddIcon />
-                        </IconButton>
-                        <Typography variant="body1" sx={{ mx: 2 }}> {count} </Typography>
-                        <IconButton onClick={() => removeItem(name)} sx={{ mx: 1, color: '#FF0000' }} >
-                          <RemoveIcon />
-                        </IconButton>
-                      </Box>
-                  </Box>
-                ))}
-              </Stack>
-            </CardContent>
-          </Card>
+      {/*Main Content*/}
+      <Container maxWidth="xl" sx={{mt:-1, ml:-3}}>
+        <Box width="100vw" height="100vh" display={"flex"} justifyContent={"center"} flexDirection={"column"} alignItems={"center"} backgroundColor={'#16213e'}>
+          <Box display={'flex'} flexDirection={{xs:'row', md:'column'}} gap={2}>
+            {/*Input Field for items*/}
+            <Card borderradius={4} boxShadow={'0 4px 8px rgba(0,0,0,0.1)'} sx={{bgcolor: '#2D2D2D'}}>
+              <CardContent>
+                <Stack width="100%" direction={'row'} spacing={2}>
+                <TextField label="Enter Item Name" variant="outlined" fullWidth value={itemName} onChange={(e) => setItemName(e.target.value)}/>
+                <Button variant="contained" color='secondary' sx={{color: '#000000'}}
+                onClick={() => {
+                  addItem(itemName) 
+                  setItemName('')
+                  handleClose()}}
+                  >Add</Button>
+                </Stack>
+              </CardContent> 
+            </Card>
+            {/*Pantry Items Display*/}
+            <Card borderradius={4} boxShadow={'0 4px 8px rgba(0,0,0,0.1)'} sx={{bgcolor: '#2D2D2D'}}>
+              <CardContent>
+                <Box width="800px" height="85px" bgcolor={'#0f0f10'} display={"flex"} justifyContent={"center"} alignItems={"center"}>
+                  <Typography variant={'h2'} color={'#ffffff'} textAlign={'center'}>
+                    Pantry Items
+                  </Typography>
+                </Box>
+                <Stack width="800px" height="500px" spacing={2} overflow={'auto'}>
+                  {pantry.map(({name, count}) => (
+                      <Box key={name} width="100%" minHeight="40px" display={"flex"} justifyContent={"space-between"} alignItems={"center"} bgcolor={'#2D2D2D'} color={'#ffffff'} paddingX={5}>
+                        <Typography variant="h6">{name.charAt(0).toUpperCase() + name.slice(1)}</Typography>
+                        <Box display="flex" alignItems="center">
+                          <IconButton onClick={() => addItem(name)} sx={{ mx: 1, color: '#008000' }}>
+                            <AddIcon />
+                          </IconButton>
+                          <Typography variant="body1" sx={{ mx: 2 }}> {count} </Typography>
+                          <IconButton onClick={() => removeItem(name)} sx={{ mx: 1, color: '#FF0000' }} >
+                            <RemoveIcon />
+                          </IconButton>
+                        </Box>
+                    </Box>
+                  ))}
+                </Stack>
+              </CardContent>
+            </Card>
+          </Box>
         </Box>
-      </Box>
+      </Container>
       {/*Footer*/}
       <Box sx={{backgroundColor: '#16213e', color: '#000000', spacing: 2, bottom: 0, width: '100%'}} padding={2}>
         <Typography variant="h6" textAlign={'left'}> © Pantry Buddy 2024 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .Made by Ammar Khan</Typography>
         <Box sx={{backgroundColor: '#16213e', color: '#000000', bottom: 0, width: '100%'}} padding={2} justifyContent={'center'} display={'flex'}>
             <a href="https://github.com/Ammar-Khan18" style={{ display: 'flex', alignItems: 'center', color: 'inherit', textDecoration: 'none' }}>
-              <GitHubIcon />
+              <GitHub />
             </a>
             <a href="https://www.linkedin.com/in/ammarbinaamirkhan/" style={{ display: 'flex', alignItems: 'center', color: 'inherit', textDecoration: 'none' }}>
-              <LinkedInIcon />
+              <LinkedIn />
             </a>
             <a href="https://ammar-khan18.github.io/Portfolio-Website/" style={{ display: 'flex', alignItems: 'center', color: 'inherit', textDecoration: 'none' }}>
-              <WebIcon />
+              <Public />
             </a>
         </Box>
       </Box>
